63dd34e418abcf65038205e3ea132af1bc031c81
louise.couture <louise.couture@etu.unistra.fr>
2021-12-06 08:38:21 +0100

    mat

--- src/program.c
+++ src/program.c
@@ -3,68 +3,9 @@
 #include "baseParser.h"
 #include "io-utils.h"
 #include "fonctionsMatrice.h"
-/*
-//int interruptFlag = 0;
 
+int interruptFlag = 0;
 
-int main(int argc, char ** argv){
-    initSigaction();
-    int exitCode;
-    options_t options;
-    exitCode=parseArgs(argc, argv, &options);
-    if(exitCode) goto error;
-    switch (options.action)
-    {
-    case ACTION_PARSE:
-        exitCode=openFiles(&options, "w");
-        if(exitCode) goto error;
-        exitCode=parseBase(&options);
-        if(exitCode) goto error;
-        printf("Database parsing ok ! \n");
-        /* char * line = malloc(1000);
-        int i=0;
-        while (fgets(line, 1000, options.inputFile) != NULL){
-            i++;
-        } */
-        break;
-    case ACTION_READ:
-        exitCode=openFiles(&options, "r");
-        if(exitCode) goto error;
-        exitCode=readEntireBin(&options);
-        if(exitCode) goto error;
-        //structureBase_t structureBase;
-        //initStructure(&structureBase, 0);
-        //structureBase = readEntryBin(&options, 7);
-        //printStruct(&structureBase);
-        break;
-    case ACTION_MAT:
-        break;
-    case ACTION_SHOW_ARTICLES:
-        exitCode=openFiles(&options, "r");
-        if(exitCode) goto error;
-        exitCode=showArticles(&options);
-        if(exitCode) goto error;
-        break;
-    case ACTION_SHOW_AUTHORS:
-        exitCode=openFiles(&options, "r");
-        if(exitCode) goto error;
-        exitCode=showAuthors(&options);
-        if(exitCode) goto error;
-        break;
-    default:
-        fprintf(stderr, "Action is missing\n");
-        goto error;
-        break;
-    }
-    closeFiles(&options);
-
-    error: if(exitCode){ 
-        fprintf(stderr, "%s\n", errorToString(exitCode));
-        return exitCode;
-    }
-    return OK;
-}
-*/
  void testCreateListeAdj(void){
     char c0[]="author0";
 
@@ -105,10 +46,68 @@ int main(int argc, char ** argv){
     freeListAdj(node0);
 }
 
-int main(void){
-    testCreateListeAdj();
-    return 0;
-} 
+int main(int argc, char ** argv){
+    initSigaction();
+    int exitCode;
+    options_t options;
+    exitCode=parseArgs(argc, argv, &options);
+    if(exitCode) goto error;
+    switch (options.action)
+    {
+    case ACTION_PARSE:
+        exitCode=openFiles(&options, "w");
+        if(exitCode) goto error;
+        exitCode=parseBase(&options);
+        if(exitCode) goto error;
+        printf("Database parsing ok ! \n");
+        /* char * line = malloc(1000);
+        int i=0;
+        while (fgets(line, 1000, options.inputFile) != NULL){
+            i++;
+        } */
+         break;
+    case ACTION_READ:
+        exitCode=openFiles(&options, "r");
+        if(exitCode) goto error;
+        exitCode=readEntireBin(&options);
+        if(exitCode) goto error;
+        //structureBase_t structureBase;
+        //initStructure(&structureBase, 0);
+        //structureBase = readEntryBin(&options, 7);
+        //printStruct(&structureBase);
+        break;
+    case ACTION_MAT:
+        testCreateListeAdj();
+        break;
+    case ACTION_SHOW_ARTICLES:
+        exitCode=openFiles(&options, "r");
+        if(exitCode) goto error;
+        exitCode=showArticles(&options);
+        if(exitCode) goto error;
+        break;
+    case ACTION_SHOW_AUTHORS:
+        exitCode=openFiles(&options, "r");
+        if(exitCode) goto error;
+        exitCode=showAuthors(&options);
+        if(exitCode) goto error;
+        break;
+    default:
+        fprintf(stderr, "Action is missing\n");
+        goto error;
+        break;
+    }
+    closeFiles(&options);
+
+    error: if(exitCode){ 
+        fprintf(stderr, "%s\n", errorToString(exitCode));
+        return exitCode;
+    }
+    return OK; 
+}
+
+
+
+
 
 /* int main(int argc, char ** argv){
     options_t options;
