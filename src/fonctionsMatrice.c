1ef49f927fe5d3330035260f4b3fdbff6fcb78c2
Felix Lusseau <felix.lusseau@etu.unistra.fr>
2022-01-03 10:35:31 +0100

    jzrkbgv

--- src/fonctionsMatrice.c
+++ src/fonctionsMatrice.c
@@ -418,27 +418,23 @@ void printDistance(int n1, node *node0) {
     printf("author:%s distance de %i:%i\n", currentNode->author, n1,
            currentNode->distance);
 }
-<<<<<<< HEAD
 
-void plusCourtChemin(int n1,int n2,node *node0,int taille){
-    dijkstra(n1,node0,taille);
-    printf("plus court chemin entre %i et %i:\n",n1,n2);
-    node *currentNode=GoToNode(n2,node0);
-    printf("n2:%s\n",currentNode->author);
-    edge *currentEdge=currentNode->nodeEdge;
-    node *minVoisin=currentEdge->otherNode;
-    while(currentNode->nodeNumber!=n1){
-        while(currentEdge!=NULL){
-            if(currentEdge->otherNode->distance < minVoisin->distance){
-                minVoisin=currentEdge->otherNode;
+void plusCourtChemin(int n1, int n2, node *node0, int taille) {
+    dijkstra(n1, node0, taille);
+    printf("plus court chemin entre %i et %i:\n", n1, n2);
+    node *currentNode = GoToNode(n2, node0);
+    printf("n2:%s\n", currentNode->author);
+    edge *currentEdge = currentNode->nodeEdge;
+    node *minVoisin = currentEdge->otherNode;
+    while (currentNode->nodeNumber != n1) {
+        while (currentEdge != NULL) {
+            if (currentEdge->otherNode->distance < minVoisin->distance) {
+                minVoisin = currentEdge->otherNode;
             }
         }
-        currentNode=minVoisin;
-        currentEdge=currentNode->nodeEdge;
-        printf("%s\n",currentNode->author);
+        currentNode = minVoisin;
+        currentEdge = currentNode->nodeEdge;
+        printf("%s\n", currentNode->author);
     }
-    printf("n1:%s\n",currentNode->author);
+    printf("n1:%s\n", currentNode->author);
 }
-
-=======
->>>>>>> fcc995f4c91a2fb5344a577e07680be43e302f3b
