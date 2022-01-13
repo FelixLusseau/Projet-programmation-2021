5b51966ae5b9b8a9f858808481fced5fe4954b33
Félix Lusseau <felix.lusseau@etu.unistra.fr>
2022-01-13 20:44:22 +0100

    tests

--- tests/tests.c
+++ tests/tests.c
@@ -1,7 +1,10 @@
 #include <math.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #include "argsParser.h"
@@ -13,9 +16,6 @@
 #include "readFunctions.h"
 #include "searchingFunctions.h"
 #include "tps_unit_test.h"
-
-#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 int interruptFlag = 0;
 
@@ -78,6 +78,108 @@ void testArgMissing() {
     tps_assert(parseArgs(argc, argv, &options) == ERROR_ARGS_PARSE);
 }
 
+void testErrorFiles() {
+    options_t options;
+    initOptions(&options);
+    int exitCode;
+    options.inputFilename = "sampl.xml";
+    options.action[ACTION_PARSE] = TO_DO;
+    tps_assert((exitCode = openFiles(&options, "w", 0)) == ERROR_OPEN_DATABASE);
+    closeFiles(&options);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    options.inputFilename = "sample.bin";
+    tps_assert((exitCode = openFiles(&options, "w", 0)) == ERROR_XML);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+
+    options.action[ACTION_PARSE] = NOT_TO_DO;
+    options.outputFilename = "out9";
+    tps_assert((exitCode = openFiles(&options, "r", 0)) == ERROR_OPEN_BIN);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    options.outputFilename = "sample.xml";
+    tps_assert((exitCode = openFiles(&options, "r", 0)) == ERROR_BIN);
+    closeFiles(&options);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+}
+
+void testMallocError1() {
+    __remaining_alloc = 0;
+    int exitCode = OK;
+    node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
+    tps_assert(hashTable == NULL);
+    if (hashTable == NULL)
+        exitCode = ERROR_HASHTABLE;
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    options_t options;
+    initOptions(&options);
+    options.inputFilename = "sample.xml";
+    options.outputFilename = "out";
+    options.action[ACTION_PARSE] = TO_DO;
+    openFiles(&options, "w", 0);
+    tps_assert((exitCode = parseBase(&options)) == ERROR_BASE_PARSE);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    closeFiles(&options);
+    __remaining_alloc = -1;
+}
+
+void testMallocError2() {
+    node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
+    tps_assert(hashTable != NULL);
+    for (int i = 0; i < HT_SIZE; i++)
+        hashTable[i] = NULL;
+    __remaining_alloc = 0;
+    options_t options;
+    initOptions(&options);
+    int exitCode = OK;
+    options.outputFilename = "sample.bin";
+    openFiles(&options, "r", 0);
+    node *node0;
+    int taille = 0;
+    tps_assert((node0 = DoListAdjDeBinHash(&options, &taille, hashTable)) ==
+               NULL);
+    if (node0 == NULL)
+        exitCode = ERROR_LIST;
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    tps_assert((exitCode = printAuthorAtDist(&options, node0)) ==
+               ERROR_NODE_EQ_NULL);
+    fprintf(stderr, "%s\n", errorToString(exitCode));
+    endOfProgram(&options, node0, hashTable);
+    __remaining_alloc = -1;
+}
+
+void testInterruption() {
+    int raison;
+    node *node0 = NULL;
+    node **hashTable = NULL;
+    options_t options;
+    initOptions(&options);
+    initSigaction();
+    switch (fork()) {
+    case -1:
+        fprintf(stderr, "Error generating the sub process\n");
+        break;
+    case 0:
+        sleep(0.9);
+        kill(getppid(), SIGINT);
+        exit(0);
+    default:
+        options.outputFilename = "outsampletest.bin";
+        openFiles(&options, "r", 0);
+        tps_assert(readEntireBin(&options, 1) == OK);
+        endOfProgram(&options, node0, hashTable);
+        if (wait(&raison) == -1)
+            fprintf(stderr, "fail wait\n");
+        if (WIFEXITED(raison))
+            fprintf(stderr, "exit(%d)\n", WEXITSTATUS(raison));
+        else if (WIFSIGNALED(raison))
+            fprintf(stderr, "signal %d\n", WTERMSIG(raison));
+        else
+            fprintf(stderr, "other reason\n");
+        tps_assert(interruptFlag == 1);
+        break;
+    }
+    interruptFlag = 0;
+}
+
 void testParseArgs() {
     printUsage();
     options_t options;
@@ -112,27 +214,6 @@ void testParseArgs() {
     tps_assert(options.action[ACTION_SHOW_ARTICLES_YEAR] == TO_DO);
 }
 
-void testErrorFiles() {
-    options_t options;
-    initOptions(&options);
-    int exitCode;
-    options.inputFilename = "sampl.xml";
-    options.action[ACTION_PARSE] = TO_DO;
-    tps_assert((exitCode = openFiles(&options, "w", 0)) == ERROR_OPEN_DATABASE);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    options.inputFilename = "sample.bin";
-    tps_assert((exitCode = openFiles(&options, "w", 0)) == ERROR_XML);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-
-    options.action[ACTION_PARSE] = NOT_TO_DO;
-    options.outputFilename = "out9";
-    tps_assert((exitCode = openFiles(&options, "r", 0)) == ERROR_OPEN_BIN);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    options.outputFilename = "sample.xml";
-    tps_assert((exitCode = openFiles(&options, "r", 0)) == ERROR_BIN);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-}
-
 void testParseBase() {
     options_t options;
     initOptions(&options);
@@ -146,57 +227,10 @@ void testParseBase() {
     closeFiles(&options);
 }
 
-void testMallocError1() {
-    __remaining_alloc = 0;
-    int exitCode = OK;
-    node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
-    tps_assert(hashTable == NULL);
-    if (hashTable == NULL)
-        exitCode = ERROR_HASHTABLE;
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    options_t options;
-    initOptions(&options);
-    options.inputFilename = "sample.xml";
-    options.outputFilename = "out";
-    options.action[ACTION_PARSE] = TO_DO;
-    openFiles(&options, "w", 0);
-    tps_assert((exitCode = parseBase(&options)) == ERROR_BASE_PARSE);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    closeFiles(&options);
-    __remaining_alloc = -1;
-}
-
-void testMallocError2() {
-    node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
-    tps_assert(hashTable != NULL);
-    for (int i = 0; i < HT_SIZE; i++)
-        hashTable[i] = NULL;
-    __remaining_alloc = 0;
-    options_t options;
-    initOptions(&options);
-    int exitCode = OK;
-    options.action[ACTION_PARSE] = NOT_TO_DO;
-    options.outputFilename = "sample.bin";
-    openFiles(&options, "r", 0);
-    node *node0;
-    int taille = 0;
-    tps_assert((node0 = DoListAdjDeBinHash(&options, &taille, hashTable)) ==
-               NULL);
-    if (node0 == NULL)
-        exitCode = ERROR_LIST;
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    tps_assert((exitCode = printAuthorAtDist(&options, node0)) ==
-               ERROR_NODE_EQ_NULL);
-    fprintf(stderr, "%s\n", errorToString(exitCode));
-    endOfProgram(&options, node0, hashTable);
-    __remaining_alloc = -1;
-}
-
 void testRead() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "outsampletest.bin";
-    options.action[ACTION_PARSE] = 0;
     openFiles(&options, "r", 0);
     tps_assert(readEntireBin(&options, 1) == OK);
     closeFiles(&options);
@@ -204,7 +238,6 @@ void testRead() {
 
 void testGraph() {
     int taille = 0;
-    node *node0 = NULL;
     node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
     tps_assert(hashTable != NULL);
     for (int i = 0; i < HT_SIZE; i++)
@@ -212,9 +245,8 @@ void testGraph() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "outsampletest.bin";
-    options.action[ACTION_PARSE] = 0;
     openFiles(&options, "r", 0);
-    node0 = DoListAdjDeBinHash(&options, &taille, hashTable);
+    node *node0 = DoListAdjDeBinHash(&options, &taille, hashTable);
     tps_assert(node0 != NULL);
     printListAdj(node0);
     endOfProgram(&options, node0, hashTable);
@@ -222,7 +254,6 @@ void testGraph() {
 
 void testArticles() {
     int taille = 0;
-    node *node0 = NULL;
     node **hashTable = malloc(HT_SIZE * sizeof(unsigned int) * sizeof(char *));
     tps_assert(hashTable != NULL);
     for (int i = 0; i < HT_SIZE; i++)
@@ -230,10 +261,9 @@ void testArticles() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "outsampletest.bin";
-    options.action[ACTION_PARSE] = 0;
     options.authorNames[0] = "Russell Turpin";
     openFiles(&options, "r", 0);
-    node0 = DoListAdjDeBinHash(&options, &taille, hashTable);
+    node *node0 = DoListAdjDeBinHash(&options, &taille, hashTable);
     tps_assert(node0 != NULL);
     tps_assert(showArticles(&options, node0, 0) == OK);
     endOfProgram(&options, node0, hashTable);
@@ -249,7 +279,6 @@ void testShortestPath() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "sample2.bin";
-    options.action[ACTION_PARSE] = 0;
     options.authorNames[0] = "Takaya Asano";
     options.authorNames[1] = "Takuya Iwamoto";
     openFiles(&options, "r", 0);
@@ -273,7 +302,6 @@ void testDistances() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "sample2.bin";
-    options.action[ACTION_PARSE] = 0;
     options.authorNames[0] = "Takaya Asano";
     options.authorNames[1] = "Takuya Iwamoto";
     options.N = 2;
@@ -300,9 +328,6 @@ void testConnected() {
     options_t options;
     initOptions(&options);
     options.outputFilename = "outsampletest.bin";
-    options.action[ACTION_PARSE] = 0;
-    options.action[ACTION_GRAPH] = TO_DO;
-    options.action[ACTION_CONNECTED] = TO_DO;
     openFiles(&options, "r", 0);
     node0 = DoListAdjDeBinHash(&options, &taille, hashTable);
     tps_assert(node0 != NULL);
@@ -329,12 +354,13 @@ int main(void) {
 
     closeFiles(&options);*/
 
-    TEST(testArgMissing);
-    TEST(testParseArgs);
     TEST(testErrorFiles);
-    TEST(testParseBase);
     TEST(testMallocError1);
     TEST(testMallocError2);
+    TEST(testArgMissing);
+    TEST(testInterruption);
+    TEST(testParseArgs);
+    TEST(testParseBase);
     TEST(testRead);
     TEST(testGraph);
     TEST(testArticles);
